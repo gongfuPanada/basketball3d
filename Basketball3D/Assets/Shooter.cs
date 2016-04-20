9a6fc7579e876dc4010cfab46fbe212567ecb3e5
Harry Y <harry+@outlook.com>
2016-04-19 22:19:25 -0700

    Sample basketball game

---
+++ Basketball3D/Assets/Shooter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour
+{
+    public Rigidbody prefab;
+    public float speed = 10.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")) {
+            var ball = (Rigidbody) Instantiate(prefab, transform.position, transform.rotation);
+            ball.velocity = (transform.forward + transform.up * 0.8f) * speed;
+        }
+    }
+
+
+
+}
